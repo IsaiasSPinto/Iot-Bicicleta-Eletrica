8979fd9c3094a1b08cfdb5c0d6596ebfb83f479e
unknown <isaiascxs10@gmail.com>
2021-11-08 22:51:49 -0300

    layout finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { database } from './services/firebase';
 import { ref, set} from "firebase/database";
 import { useState } from 'react';
-import './styles/globalstyles.css'
+import './globalStyles.css'
+import './styles/app.css'
+import logo from './assets/images/marcopolo-logo.png'
 
 
 
@@ -10,7 +12,7 @@ import './styles/globalstyles.css'
 function App() {
 
   
-  const [ledOn, setLedOn] = useState(0)
+  const [ledOn, setLedOn] = useState(1)
 
 
   function checkDb(){
@@ -41,9 +43,35 @@ function App() {
   }
 
   return (
+    <>
+      <header className="header">
+        <img src={logo} />
+      </header>
 
-    <button style={ledOn == 1 ? redStyle : greenStyle } className="botaoPipoqueira" onClick={checkDb}>{ledOn == 1 ? "Pipoqueira Desligada" : "Pipoqueira Ligada"}</button>
+      <main className="first-content">
+        <h1>Feira de trabalhos <br /> EFPM 2021</h1>
+        <div className="bike-section">
+          <h2>Bicicleta Eletrica</h2>
+          <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti blanditiis rem ipsum iste inventore magnam quos nam similique cupiditate ullam corporis, velit nesciunt asperiores nemo! Minima in perspiciatis provident nesciunt?</span>
+          {/* <img src="" alt="" /> */}
+        </div>
+      </main>
+      <section className="second-content">
+        <div className="energy-section">
+          <h2>Energia Limpa</h2>
+          <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem velit labore delectus voluptatum beatae maiores. Neque, explicabo? Laborum, neque eos recusandae nulla eligendi ipsam. Blanditiis asperiores deserunt atque ducimus aspernatur.</span>
+          {/* <img src="" alt="" /> */}
+        </div>
+      </section> 
+      <div className="button"> 
+        <button style={ledOn == 1 ? redStyle : greenStyle } className="botaoPipoqueira" onClick={checkDb}>{ledOn == 1 ? "Pipoqueira Desligada" : "Pipoqueira Ligada"}</button>
+      </div>
 
+      <footer className="footer">
+        <span>Trabalho feito por...</span>
+      </footer>
+      
+    </>
   );
 }
 
